da5d76c7046bcada98f3d05466417de90cfb7c6e
Jessy1237 <j.e.s.s.e.b.o.yvs@gmail.com>
2018-02-13 12:21:50 +0800

    Restored functionality of Vanilla Race

--- src/com/Jessy1237/DwarfCraft/listeners/DwarfListener.java
+++ src/com/Jessy1237/DwarfCraft/listeners/DwarfListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.Jessy1237.DwarfCraft.DwarfCraft;
+import com.Jessy1237.DwarfCraft.events.DwarfEffectEvent;
 import com.Jessy1237.DwarfCraft.events.DwarfLevelUpEvent;
 import com.Jessy1237.DwarfCraft.models.DwarfPlayer;
 import com.Jessy1237.DwarfCraft.models.DwarfSkill;
@@ -20,7 +21,7 @@ public class DwarfListener implements Listener
     }
 
     @EventHandler( priority = EventPriority.NORMAL, ignoreCancelled = true )
-    public void onDwarfCraftLevelUp( DwarfLevelUpEvent event )
+    public void onDwarfLevelUp( DwarfLevelUpEvent event )
     {
         DwarfPlayer player = event.getDwarfPlayer();
         DwarfSkill skill = event.getSkill();
@@ -29,6 +30,18 @@ public class DwarfListener implements Listener
         {
             String name = plugin.getChat().getPlayerPrefix( player.getPlayer() ) + player.getPlayer().getName() + plugin.getChat().getPlayerSuffix( player.getPlayer() );
             plugin.getOut().sendBroadcast( plugin.getConfigManager().getAnnouncementMessage().replace( "%playername%", name ).replace( "%skillname%", skill.getDisplayName() ).replace( "%level%", "" + skill.getLevel() ) );
+        }
+    }
+
+    public void onDwarfEffectEvent( DwarfEffectEvent event )
+    {
+        DwarfPlayer player = event.getDwarfPlayer();
+        if ( player == null )
+            return;
+        
+        if ( player.getRace().equalsIgnoreCase( plugin.getConfigManager().getVanillaRace() ) )
+        {
+            event.setCancelled( true );
         }
     }
 }
